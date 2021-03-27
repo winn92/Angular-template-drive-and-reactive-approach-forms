61f3c773fd371340f31c0dbf9073c474b02e4ead
erwin <winn92091229@gmail.com>
2021-03-27 13:22:03 -0400

    13.TD-Using-Form-Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,17 @@ export class AppComponent {
   defaultQuestion="pet";
   answer="";
   genders=['male','female'];
+
+  user={
+    username:'',
+    email:'',
+    secretQuestion:'',
+    answer:'',
+    gender:''
+  }
+  
+  submitted=false;
+
   // onSubmit(form:NgForm){
   //   console.log(form);
   // }
@@ -36,6 +47,11 @@ export class AppComponent {
   }
 
   onSubmit(){
-    console.log(this.signupForm);
+    this.submitted=true;
+    this.user.username=this.signupForm.value.userData.username;
+    this.user.email=this.signupForm.value.userData.email;
+    this.user.secretQuestion=this.signupForm.value.secret;
+    this.user.answer=this.signupForm.value.questionAnswer;
+    this.user.gender=this.signupForm.value.gender;
   }
 }
