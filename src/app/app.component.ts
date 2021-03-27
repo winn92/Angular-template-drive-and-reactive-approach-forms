32b1d5525a20547747c57e859c01601db8457438
erwin <winn92091229@gmail.com>
2021-03-27 13:27:24 -0400

    14.TD-Resetting-Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -53,5 +53,6 @@ export class AppComponent {
     this.user.secretQuestion=this.signupForm.value.secret;
     this.user.answer=this.signupForm.value.questionAnswer;
     this.user.gender=this.signupForm.value.gender;
+    this.signupForm.reset();
   }
 }
