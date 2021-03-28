213102a893c57ace92539e07799f766bb1130316
erwin <winn92091229@gmail.com>
2021-03-28 13:34:57 -0400

    21.Reactive-Grouping-Controls

--- src/app/reactive-approach.components.ts
+++ src/app/reactive-approach.components.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, NgForm, NgModel, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, NgForm, NgModel, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -13,13 +13,25 @@ export class AppComponent implements OnInit{
 
   ngOnInit(): void {
     this.signupForm=new FormGroup({
-      'username':new FormControl(null,Validators.required),
-      'email':new FormControl(null,[Validators.required,Validators.email]),
+      'userData':new FormGroup({
+        'username':new FormControl(null,Validators.required),
+        'email':new FormControl(null,[Validators.required,Validators.email]),
+      }),
       'gender':new FormControl('male'),
+      // 'hobbies':new FormArray([])
     });
   }
+
+  /* onAddHobby(){
+    const control=new FormControl(null,Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  } */
 
   onSubmit(){
     console.log(this.signupForm);
   }
+
+  /* get controls() {
+    return (this.signupForm.get('hobbies') as FormArray).controls;
+  } */
 }
