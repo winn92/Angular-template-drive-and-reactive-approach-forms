1f8290975376193645af139920f032f0a51ad2c6
erwin <winn92091229@gmail.com>
2021-04-03 13:21:52 -0400

    Reactive: Reacting to Status or Value Changes

--- src/app/reactive-approach.components.ts
+++ src/app/reactive-approach.components.ts
@@ -22,6 +22,9 @@ export class AppComponent implements OnInit{
       'gender':new FormControl('male'),
       'hobbies':new FormArray([])
     });
+
+    this.signupForm.valueChanges.subscribe(value=>console.log(value));
+    this.signupForm.statusChanges.subscribe(value=>console.log(value));
   }
 
   onAddHobby(){
